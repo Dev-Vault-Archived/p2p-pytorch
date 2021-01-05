10aae190a5a9ac6e691618ecf7926b2c4b2de4c8
Galih Akbar Moerbayaksa <moer@student.uns.ac.id>
2021-01-05 17:20:06 +0700

    fix

--- train.py
+++ train.py
@@ -183,7 +183,7 @@ if __name__ == '__main__':
             # Masking real_a and fake_b
 
             # First, G(A) should fake the discriminator
-            fake_ab = torch.cat((real_a, np.bitwise_and(fake_b.cpu(), real_a.cpu()).to(device)), 1)
+            fake_ab = torch.cat((real_a, np.bitwise_and(fake_b.detach().numpy(), real_a.detach().numpy()).to(device)), 1)
             pred_fake = net_d.forward(fake_ab)
             loss_g_gan = criterionGAN(pred_fake, True)
 
