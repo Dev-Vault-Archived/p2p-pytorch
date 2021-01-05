6470bc1fee0d5522bfe314d3b4aec4d617dea0c8
Galih Akbar Moerbayaksa <moer@student.uns.ac.id>
2021-01-05 21:52:07 +0700

    back

--- train.py
+++ train.py
@@ -228,18 +228,18 @@ if __name__ == '__main__':
             # loss_g += loss_sobelL1
 
             # Perceptual loss
-            # target_content_features = extract_features(criterionVGG, real_b, [15])
+            target_content_features = extract_features(criterionVGG, real_b, [15])
             target_style_features = extract_features(criterionVGG, real_b, [3, 8, 15, 22]) 
 
-            # output_content_features = extract_features(criterionVGG, fake_b, [15])
+            output_content_features = extract_features(criterionVGG, fake_b, [15])
             output_style_features = extract_features(criterionVGG, fake_b, [3, 8, 15, 22])
 
             style_loss = calc_Gram_Loss(output_style_features, target_style_features)
-            # content_loss = calc_c_loss(output_content_features, target_content_features)
-            # tv_loss = calc_tv_Loss(fake_b)
+            content_loss = calc_c_loss(output_content_features, target_content_features)
+            tv_loss = calc_tv_Loss(fake_b)
 
-            # loss_g += content_loss * 1.0 + tv_loss * 1.0 + style_loss * 30.0
-            loss_g += style_loss * 10.0
+            loss_g += content_loss * 1.0 + tv_loss * 1.0 + style_loss * 10.0
+            # loss_g += style_loss * 10.0
 
             loss_g.backward()
 
@@ -254,12 +254,9 @@ if __name__ == '__main__':
                 loss_g.item(),
                 loss_G_GAN_Feat.item(),
                 loss_G_Ang.item(),
-                0,
+                content_loss.item(),
                 style_loss.item(),
-                0,
-                # content_loss.item(),
-                # style_loss.item(),
-                # tv_loss.item()
+                tv_loss.item()
             ))
             # print("===> Epoch[{}]({}/{}): Loss_D: {:.4f} Loss_G: {:.4f} Loss_GFeat: {:.4f} Loss_Sobel: {:.4f} Loss_Perp: {:.4f} Loss_TV: {:.4f}".format(
                 # epoch, iteration, len(training_data_loader), loss_d.item(), loss_g.item(), loss_G_GAN_Feat.item(), loss_sobelL1.item(), content_loss.item(), tv_loss.item()))
