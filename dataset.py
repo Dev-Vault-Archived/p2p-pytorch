7e84892e33194062db8dc456e3a4da6a839bfab6
Galih Akbar Moerbayaksa <moer@student.uns.ac.id>
2021-01-11 11:48:01 +0700

    fix dataset

--- dataset.py
+++ dataset.py
@@ -1,6 +1,6 @@
 from os import listdir
 from os.path import join
-import random
+# import random
 
 from PIL import Image
 import torch
@@ -8,9 +8,6 @@ import torch.utils.data as data
 import torchvision.transforms as transforms
 
 from utils import is_image_file, load_img
-
-IMAGENET_MEAN = (0.5, 0.5, 0.5)
-IMAGENET_STD = (0.5, 0.5, 0.5)
 
 class DatasetFromFolder(data.Dataset):
     def __init__(self, image_dir, direction):
@@ -21,32 +18,32 @@ class DatasetFromFolder(data.Dataset):
         self.image_filenames = [x for x in listdir(self.a_path) if is_image_file(x)]
 
         transform_list = [transforms.ToTensor(),
-                          transforms.Normalize(IMAGENET_MEAN, IMAGENET_STD)]
+                          transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]
 
         self.transform = transforms.Compose(transform_list)
 
     def __getitem__(self, index):
         a = Image.open(join(self.a_path, self.image_filenames[index])).convert('RGB')
         b = Image.open(join(self.b_path, self.image_filenames[index])).convert('RGB')
-        a = a.resize((286, 286), Image.BICUBIC)
-        b = b.resize((286, 286), Image.BICUBIC)
+        # a = a.resize((286, 286), Image.BICUBIC)
+        # b = b.resize((286, 286), Image.BICUBIC)
         
         a = transforms.ToTensor()(a)
         b = transforms.ToTensor()(b)
-        w_offset = random.randint(0, max(0, 286 - 256 - 1))
-        h_offset = random.randint(0, max(0, 286 - 256 - 1))
+        # w_offset = random.randint(0, max(0, 286 - 256 - 1))
+        # h_offset = random.randint(0, max(0, 286 - 256 - 1))
     
-        a = a[:, h_offset:h_offset + 256, w_offset:w_offset + 256]
-        b = b[:, h_offset:h_offset + 256, w_offset:w_offset + 256]
+        # a = a[:, h_offset:h_offset + 256, w_offset:w_offset + 256]
+        # b = b[:, h_offset:h_offset + 256, w_offset:w_offset + 256]
     
-        a = transforms.Normalize(IMAGENET_MEAN, IMAGENET_STD)(a)
-        b = transforms.Normalize(IMAGENET_MEAN, IMAGENET_STD)(b)
+        a = transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))(a)
+        b = transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))(b)
 
-        if random.random() < 0.5:
-            idx = [i for i in range(a.size(2) - 1, -1, -1)]
-            idx = torch.LongTensor(idx)
-            a = a.index_select(2, idx)
-            b = b.index_select(2, idx)
+        # if random.random() < 0.5:
+        #     idx = [i for i in range(a.size(2) - 1, -1, -1)]
+        #     idx = torch.LongTensor(idx)
+        #     a = a.index_select(2, idx)
+        #     b = b.index_select(2, idx)
 
         if self.direction == "a2b":
             return a, b
