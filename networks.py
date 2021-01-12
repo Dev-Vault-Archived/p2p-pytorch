c9079d09a392d83de8cc8b12db92b93799d2294b
Galih Akbar Moerbayaksa <moer@student.uns.ac.id>
2021-01-12 11:30:27 +0700

    change

--- networks.py
+++ networks.py
@@ -243,7 +243,7 @@ class ConvLayer(nn.Module):
 
 class DeConvLayer(nn.Module):
     def __init__(self, in_channels, out_channels, kernel_size, stride):
-        super(ConvLayer, self).__init__()
+        super(DeConvLayer, self).__init__()
         padding = kernel_size // 2
         # self.reflection_pad = nn.ReflectionPad2d(padding)
         self.conv2d = nn.ConvTranspose2d(in_channels, out_channels, kernel_size, stride, padding=padding) #, padding)
