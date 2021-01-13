dfe2a8c6914831944d33c2ff5b735515896935d1
Galih Akbar Moerbayaksa <moer@student.uns.ac.id>
2021-01-13 18:46:48 +0700

    change

--- networks.py
+++ networks.py
@@ -180,19 +180,19 @@ class CompressionNetwork(nn.Module):
         )
 
         self.conv_block1 = nn.Sequential(
-            ConvLayer(64, 64, kernel_size=3, stride=1),
+            ConvLayer(64, 64, kernel_size=2, stride=2),
             nn.BatchNorm2d(64),
             nn.LeakyReLU(0.2)
         )
 
         self.conv_block2 = nn.Sequential(
-            ConvLayer(64, 64, kernel_size=3, stride=1),
+            ConvLayer(64, 64, kernel_size=2, stride=1),
             nn.BatchNorm2d(64),
             nn.LeakyReLU(0.2)
         )
 
         self.conv_block3 = nn.Sequential(
-            ConvLayer(64, 64, kernel_size=3, stride=1),
+            ConvLayer(64, 64, kernel_size=2, stride=1),
             nn.BatchNorm2d(64),
             nn.LeakyReLU(0.2)
         )
