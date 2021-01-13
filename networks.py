d30443e32e75cd79376ca4d64eaae63abc3ba066
Galih Akbar Moerbayaksa <moer@student.uns.ac.id>
2021-01-13 18:47:24 +0700

    change

--- networks.py
+++ networks.py
@@ -180,7 +180,7 @@ class CompressionNetwork(nn.Module):
         )
 
         self.conv_block1 = nn.Sequential(
-            ConvLayer(64, 64, kernel_size=2, stride=2),
+            ConvLayer(64, 64, kernel_size=3, stride=2),
             nn.BatchNorm2d(64),
             nn.LeakyReLU(0.2)
         )
